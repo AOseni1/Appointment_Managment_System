1ba970ef83ccbf594a5d1882a6e761abe7bd09b7
AbisolaOseni <abisola.oseni@gmail.com>
2022-07-13 21:49:25 -0700

    commit

--- src/Controller/Login.java
+++ src/Controller/Login.java
@@ -1,4 +1,56 @@
 package Controller;
 
-public class Login {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Login implements Initializable {
+    Stage stage;
+    Parent scene;
+
+    @FXML
+    private TextField userNameTextField;
+
+    @FXML
+    private PasswordField passwordTextField;
+
+    @FXML
+    private Label timeZoneLabel;
+
+    @FXML
+    private MenuButton languagePullDown;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    void onActionLogin(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/View/MainScreen.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    @FXML
+    void onActionReset(ActionEvent event) {
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/Controller/MainScreen.java
+++ src/Controller/MainScreen.java
@@ -1,4 +1,71 @@
 package Controller;
 
-public class MainScreen {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainScreen implements Initializable {
+
+    @FXML
+    private Button addCustomer;
+
+    @FXML
+    private Button modifyCustomer;
+
+    @FXML
+    private Button addAppointment;
+
+    @FXML
+    private RadioButton viewByMonth;
+
+    @FXML
+    private Button report;
+
+    @FXML
+    private RadioButton viewByWeek;
+
+    @FXML
+    private Label timeZone;
+
+    @FXML
+    private RadioButton viewAll;
+
+    @FXML
+    private Button modifyAppointment;
+
+    @FXML
+    void onActionAddCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionModifyCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionAddAppointment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionModifyAppointment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionReport(ActionEvent event) {
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
 }
